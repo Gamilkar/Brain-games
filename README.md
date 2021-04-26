706be6c577548f02aa3211166e3f3650372f7b0e
Gamilkar <gamilkarr@gmail.com>
2021-04-26 13:30:32 +0300

    step_5

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/f187347f9aa6256ee6ba/maintainability)](https://codeclimate.com/github/Gamilkar/python-project-lvl1/maintainability)
 
 [![Github Actions Status](https://github.com/Gamilkar/python-project-lvl1/actions/workflows/lint.yml/badge.svg)](https://github.com/Gamilkar/python-project-lvl1/actions)
+
+https://asciinema.org/a/tTTqWrRu9t4zKZDodSWtzQmf2
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,31 +2,33 @@
 import prompt
 import random
 
+
+def get_correct(number):
+    if number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    for i in range(3):
+    for step in range(3):
         number = random.randint(0, 1000)
+        correct = get_correct(number)
         print(f'Question: {number}')
         answer = input('Your answer: ')
-        check = ''
-        if number % 2 == 0:
-            check = 'yes'
-        elif number % 2 != 0:
-            check = 'no'
-        if answer == check:
-            if i == 2:
-                print(f'Congratulations, {name}!')
-            else:
-                print('Correct!')
-            continue
-        else:
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{check}".')
-            print(f"Let's try again, {name}!")
+        if answer != correct:
+            print((f"'{answer}' is wrong answer ;(. Correct answer "
+                   f"was '{correct}'.\nLet's try again, {name}!"))
             break
+        if answer == correct:
+            if step != 2:
+                print('Correct!')
+                continue
+            print(f'Congratulations, {name}!')
 
 
 if __name__ == '__main__':
