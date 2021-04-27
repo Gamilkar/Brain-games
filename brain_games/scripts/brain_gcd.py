9490b63bfd1c4f5969905a51b202efa2308509bf
Gamilkar <gamilkarr@gmail.com>
2021-04-27 11:29:08 +0300

    lint fix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -18,21 +18,16 @@ def get_correct(first_number, second_number):
             second_number %= first_number
     return first_number + second_number
 
-def main():
-    
-    name = welcome_user()
 
+def main():
+    name = welcome_user()
     print('Find the greatest common divisor of given numbers.')
-    
     for step in range(3):
         number_one = random.randint(1, 100)
         number_two = random.randint(1, 10)
-
         correct = get_correct(number_one, number_two)
-
         print(f'Question: {number_one} {number_two}')
         answer = int(input('Your answer: '))
-
         if answer != correct:
             print((f"'{answer}' is wrong answer ;(. Correct answer "
                    f"was '{correct}'.\nLet's try again, {name}!"))
